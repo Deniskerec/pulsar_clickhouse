f2c29b2ef3f4ac34f59ac18b072833b4ae55024d
Denis <ddeniskerec@gmail.com>
2024-03-09 12:44:41 +0100

    Remove ignored files

--- data_migration/db_info/db_backup_info.py
+++
@@ -1,6 +0,0 @@
-db_backup_info = {
-    'username': 'postgres',
-    'password': 'mac240!',
-    'host': '10.229.236.15',
-    'database_name': 'hafilafcdev',
-}
--- data_migration/db_info/remote_con.py
+++
@@ -1,4 +0,0 @@
-DB_NAME = "hafilafcdev"
-USER = "postgres"
-PASSWORD = "mac240!"
-HOST = "hafilafcdev"
