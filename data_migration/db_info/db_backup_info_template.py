c7f56b65c0ac405fd04d813af2ce796dcc71649d
Denis <ddeniskerec@gmail.com>
2024-03-09 14:04:50 +0100

    added db_info templates

---
+++ data_migration/db_info/db_backup_info_template.py
@@ -0,0 +1,8 @@
+# data_migration/db_info/db_backup_info.py
+
+db_backup_info = {
+    'username': 'your_database_username',
+    'password': 'your_database_password',
+    'host': 'your_database_host',
+    'database_name': 'your_database_name',
+}
---
+++ data_migration/db_info/remote_con_template.py
@@ -0,0 +1,5 @@
+# data_migration/db_info/remote_con.py
+DB_NAME = "database_name"
+USER = "username"
+PASSWORD = "password!"
+HOST = "host"
